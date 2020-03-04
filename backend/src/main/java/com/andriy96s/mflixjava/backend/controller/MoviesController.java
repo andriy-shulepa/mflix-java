a3aa2c466118cd7bdf28969eb7bad07d7bac38a4
andriy96s <shulepaa@gmail.com>
2020-03-04 09:43:14 +0100

    Enabled CORS for local frontend app

--- backend/src/main/java/com/andriy96s/mflixjava/backend/controller/MoviesController.java
+++ backend/src/main/java/com/andriy96s/mflixjava/backend/controller/MoviesController.java
@@ -4,6 +4,7 @@ import com.andriy96s.mflixjava.backend.dto.BasicMovieDto;
 import com.andriy96s.mflixjava.backend.service.MoviesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3001")
 @RequestMapping("/api/movies")
 public class MoviesController {
 
